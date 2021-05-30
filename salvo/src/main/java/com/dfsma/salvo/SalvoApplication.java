41eb3915056ee2e8ab9e55ad8d9846ce3274204d
Diego <diegof.df58@gmail.com>
2021-05-30 18:54:05 -0400

    add sample data

--- salvo/src/main/java/com/dfsma/salvo/SalvoApplication.java
+++ salvo/src/main/java/com/dfsma/salvo/SalvoApplication.java
@@ -3,9 +3,11 @@ package com.dfsma.salvo;
 import com.dfsma.salvo.models.Game;
 import com.dfsma.salvo.models.GamePlayer;
 import com.dfsma.salvo.models.Player;
+import com.dfsma.salvo.models.Ship;
 import com.dfsma.salvo.repositories.GamePlayerRepository;
 import com.dfsma.salvo.repositories.GameRepository;
 import com.dfsma.salvo.repositories.PlayerRepository;
+import com.dfsma.salvo.repositories.ShipRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,7 +28,7 @@ public class SalvoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(PlayerRepository repoPlayer, GamePlayerRepository repoGamePlayer, GameRepository repoGame){
+	public CommandLineRunner initData(PlayerRepository repoPlayer, GamePlayerRepository repoGamePlayer, GameRepository repoGame, ShipRepository repoShip){
 		return (args) -> {
 
 			Player player1 = new Player("Diego", "diego@gmail.com");
@@ -37,8 +39,6 @@ public class SalvoApplication {
 
 			LocalDateTime date = LocalDateTime.now();
 			Game game1 = new Game(date);
-
-			/*Local Data + 1 hrs (game2)*/
 			date = LocalDateTime.from(date.plusSeconds(3600));
 			Game game2 = new Game(date);
 			date = LocalDateTime.from(date.plusSeconds(3600));
@@ -55,11 +55,19 @@ public class SalvoApplication {
 			GamePlayer gamePlayer5 = new GamePlayer(player1, game3);
 			GamePlayer gamePlayer6 = new GamePlayer(player4, game3);
 
-
+			/*Game 1 -> Player1 Ships*/
+			Ship ship1 = new Ship("destroyer", gamePlayer1, Arrays.asList("H1","H2","H4"));
+			Ship ship2 = new Ship("Submarine", gamePlayer1, Arrays.asList("E1","F1","G1"));
+			Ship ship3 = new Ship("Patrol Boat", gamePlayer1, Arrays.asList("B4","B5"));
+			/*Game 2 -> Player2 Ships*/
+			Ship ship4 = new Ship("destroyer", gamePlayer2, Arrays.asList("H3","H4","H5"));
+			Ship ship5 = new Ship("Submarine", gamePlayer2, Arrays.asList("E4","F4","G4"));
+			Ship ship6 = new Ship("Patrol Boat", gamePlayer2, Arrays.asList("B6","B7"));
 
 			repoPlayer.saveAll(Arrays.asList(player1,player2,player3,player4));
 			repoGame.saveAll(Arrays.asList(game1,game2, game3));
 			repoGamePlayer.saveAll(Arrays.asList(gamePlayer1,gamePlayer2, gamePlayer3, gamePlayer4, gamePlayer5, gamePlayer6));
+			repoShip.saveAll(Arrays.asList(ship1,ship2,ship3,ship4,ship5,ship6));
 
 
 		};
